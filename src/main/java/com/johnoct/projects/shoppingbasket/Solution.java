a081bd61620c3b3b58347f897b0d1b233c4dbb1a
John Octubre <johnoctubre7@gmail.com>
2018-01-28 22:20:17 -0500

    initial commit

---
+++ src/main/java/com/johnoct/projects/shoppingbasket/Solution.java
@@ -0,0 +1,9 @@
+package com.johnoct.projects.shoppingbasket;
+
+/**
+ * @author John Octubre
+ * @date 1/28/2018
+ */
+public class Solution {
+
+}
---
+++ src/test/java/com/johnoct/projects/shoppingbasketTest/SolutionTest.java
@@ -0,0 +1,8 @@
+package com.johnoct.projects.shoppingbasketTest;
+
+/**
+ * @author John Octubre
+ * @date 1/28/2018
+ */
+public class SolutionTest {
+}
